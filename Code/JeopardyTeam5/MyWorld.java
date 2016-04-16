8424675d594c88319d542a6d7fc8e600b0333529
TusharaA <tushara.sies@gmail.com>
2016-04-15 21:59:03 -0700

    Rectified compile time errors

--- Code/JeopardyTeam5/MyWorld.java
+++ Code/JeopardyTeam5/MyWorld.java
@@ -17,16 +17,18 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x454 cells with a cell size of 1x1 pixels.
         super(600,454,1);
-        addObject(new Button(), 66, 127);
-        addObject(new Button(), 158, 127);
-        addObject(new Button(), 256, 127);
-        addObject(new Button(), 349, 129);
-        addObject(new Button(), 443, 127);
-        addObject(new Button(), 66, 198);
-        addObject(new Button(), 158, 198);
-        addObject(new Button(), 256, 198);
-        addObject(new Button(), 349, 198);
-        addObject(new Button(), 443, 198);
+        addObject(new Button("100"), 66, 127);
+        addObject(new Button("100"), 158, 127);
+        addObject(new Button("100"), 256, 127);
+        addObject(new Button("100"), 349, 129);
+        addObject(new Button("100"), 443, 127);
+        addObject(new Button("100"), 537, 127);
+        addObject(new Button("200"), 66, 198);
+        addObject(new Button("200"), 158, 198);
+        addObject(new Button("200"), 256, 198);
+        addObject(new Button("200"), 349, 198);
+        addObject(new Button("200"), 443, 198);
+        addObject(new Button("200"), 537, 198);
        addObject(new Score("Score: "),230,446); //Score counter
         
     }
