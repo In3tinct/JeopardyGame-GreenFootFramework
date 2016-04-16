b306a49a3f37306f06097cb3ae3eff29c2ebf28b
ChandanaRao <chandanarao.halsuligesrinivasrao@sjsu.edu>
2016-04-15 20:18:50 -0700

    Update QuestionScreen.java

--- Code/JeopardyTeam5/QuestionScreen.java
+++ Code/JeopardyTeam5/QuestionScreen.java
@@ -1,23 +1,32 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import java.awt.Color;
+//import java.awt.*;
+import java.awt.event.*;
 /**
- * Write a description of class QuestionScreen here.
- * 
- * @author (your name) 
+ * Write a description of class TextField here.
+ *
+ * @author (your name)
  * @version (a version number or a date)
  */
-public class QuestionScreen extends Actor
+public class Question extends Actor
 {
-     /**
-     * Act - do whatever the QuestionScreen wants to do. This method is called whenever
+    GreenfootImage g;
+    String ans = "";
+    
+    public Question(String text) {
+        ans = text;
+        g = new GreenfootImage(text, 30, Color.YELLOW , null);
+        setImage(g);
+        
+    }
+    
+    /**
+     * Act - do whatever the TextField wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act() 
+    public void act()
     {
-  
-        if(Greenfoot.mouseClicked(this)){
-            getWorld().removeObjects(getWorld().getObjects(TextField.class));
-           getWorld().removeObject(this);
-        }
-    }    
+        
+    }
+    
 }
