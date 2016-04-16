2dbb5917c6131a819ce37f5e6267d069fe981e87
TusharaA <tushara.sies@gmail.com>
2016-04-15 22:19:42 -0700

    Implementation of composite design pattern

--- Code/JeopardyTeam5/MyWorld.java
+++ Code/JeopardyTeam5/MyWorld.java
@@ -17,18 +17,30 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x454 cells with a cell size of 1x1 pixels.
         super(600,454,1);
-        addObject(new Button("100"), 66, 127);
-        addObject(new Button("100"), 158, 127);
-        addObject(new Button("100"), 256, 127);
-        addObject(new Button("100"), 349, 129);
-        addObject(new Button("100"), 443, 127);
-        addObject(new Button("100"), 537, 127);
-        addObject(new Button("200"), 66, 198);
-        addObject(new Button("200"), 158, 198);
-        addObject(new Button("200"), 256, 198);
-        addObject(new Button("200"), 349, 198);
-        addObject(new Button("200"), 443, 198);
-        addObject(new Button("200"), 537, 198);
+        addObject(new QuestionCardLeaf("100"), 66, 127);
+        addObject(new QuestionCardLeaf("100"), 158, 127);
+        addObject(new QuestionCardLeaf("100"), 256, 127);
+        addObject(new QuestionCardLeaf("100"), 349, 127);
+        addObject(new QuestionCardLeaf("100"), 443, 127);
+        addObject(new QuestionCardLeaf("100"), 537, 127);
+        addObject(new QuestionCardLeaf("200"), 66, 198);
+        addObject(new QuestionCardLeaf("200"), 158, 198);
+        addObject(new QuestionCardLeaf("200"), 256, 198);
+        addObject(new QuestionCardLeaf("200"), 349, 198);
+        addObject(new QuestionCardLeaf("200"), 443, 198);
+        addObject(new QuestionCardLeaf("200"), 537, 198);
+        addObject(new QuestionCardLeaf("300"), 66, 269);
+        addObject(new QuestionCardLeaf("300"), 158, 269);
+        addObject(new QuestionCardLeaf("300"), 256, 269);
+        addObject(new QuestionCardLeaf("300"), 349, 269);
+        addObject(new QuestionCardLeaf("300"), 443, 269);
+        addObject(new QuestionCardLeaf("300"), 537, 269);
+        addObject(new QuestionCardLeaf("400"), 66, 340);
+        addObject(new QuestionCardLeaf("400"), 158, 340);
+        addObject(new QuestionCardLeaf("400"), 256, 340);
+        addObject(new QuestionCardLeaf("400"), 349, 340);
+        addObject(new QuestionCardLeaf("400"), 443, 340);
+        addObject(new QuestionCardLeaf("400"), 537, 340);
        addObject(new Score("Score: "),230,446); //Score counter
         
     }
