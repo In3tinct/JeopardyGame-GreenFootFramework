f0d548a526bad5dc85fe56306e42fe4a0b9dfe7c
Chandana <chandanarao.halsuligesrinivasrao@sjsu.edu>
2016-04-21 13:32:03 -0700

    Created question cards using factory classes

--- Code/JeopardyTeam5/MainTopicComposite.java
+++ Code/JeopardyTeam5/MainTopicComposite.java
@@ -13,43 +13,37 @@ public class MainTopicComposite extends Actor implements ICardComponent
     int xcoords[] = {66,158,256,349,443,537};
     int ycoords[] = {127,198,269,340,411};
     String score[] = {"100","200","300","400","500"};
-    
+    private String categoryName;
     ArrayList<ICardComponent> questionCards = new ArrayList<>();
+    QuestionCardFactory factory = new QuestionCardFactory();
     
+    public MainTopicComposite(){
+    }
     /**
      * Act - do whatever the MainTopicComposite wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act() 
-    {
-        
-          
-            if(Greenfoot.mouseClicked(this)){
-                getWorld().addObject(new GameScreen(),300,225);
-                createAllQCards();
-                addAllQCardsToWorld();
-       
-       getWorld().addObject(new Score("Score: "),230,446);
+    public void act(){
+        if(Greenfoot.mouseClicked(this)){
             //Removing the first screen object
-
             getWorld().removeObjects(getWorld().getObjects(MainScreen.class));
+            getWorld().addObject(new GameScreen(),300,225);
+            createAllQCards(this.getName());
+            addAllQCardsToWorld();
+            getWorld().addObject(new Score("Score: "),230,446);
         }
-        // Add your action code here.
-    }  
+   }  
     
-    void createAllQCards() 
-    {
+    void createAllQCards(String categoryType){
         for(int y=0, sc=0;y<ycoords.length;y++, sc++) {
-
-            for(int x=0;x<xcoords.length;x++) {
-                ICardComponent ques = new QuestionCardLeaf(score[sc]); 
+           for(int x=0;x<xcoords.length;x++) {
+                ICardComponent ques = factory.generateQCard(categoryType,score[sc]);
                 addQCard(ques);
-            }
+           }
         }
     }
     
-    void addAllQCardsToWorld()
-    {
+    void addAllQCardsToWorld(){
         int pos = 0;
         for(int y=0, sc=0;y<ycoords.length;y++, sc++) {
 
@@ -62,28 +56,29 @@ public class MainTopicComposite extends Actor implements ICardComponent
         }
     }
     
-    void addQCardToWorld(ICardComponent ques, int x, int y)
-    {
+    void addQCardToWorld(ICardComponent ques, int x, int y){
         Actor q = (Actor)ques;
         getWorld().addObject(q, xcoords[x], ycoords[y]);
     }
     
-    public void addQCard(ICardComponent ques)
-    {
+    public void addQCard(ICardComponent ques){
         questionCards.add(ques);
     }
     
-    public void removeQCard(ICardComponent ques)
-    {
+    public void removeQCard(ICardComponent ques){
         questionCards.remove(ques);
     }
     
-    public void displayQCard()
-    {            
+    public void displayQCard(){}
+    
+    public ICardComponent getQCard(int pos){
+        return questionCards.get(pos);
     }
     
-    public ICardComponent getQCard(int pos)
-    {
-        return questionCards.get(pos);
+    public void setName(String name){
+        categoryName=name;
+    }
+    public String getName(){
+        return categoryName;
     }
 }
