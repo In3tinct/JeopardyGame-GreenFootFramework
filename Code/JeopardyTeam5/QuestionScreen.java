e45890a5034ca9d7ca69ec021e0e8a2bd3fb697f
Chandana <chandanarao.halsuligesrinivasrao@sjsu.edu>
2016-04-22 13:39:44 -0700

    Updated to refactor state design pattern

--- Code/JeopardyTeam5/QuestionScreen.java
+++ Code/JeopardyTeam5/QuestionScreen.java
@@ -2,27 +2,19 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * Write a description of class QuestionScreen here.
- *
- * @author (your name)
+ * 
+ * @author (your name) 
  * @version (a version number or a date)
  */
 public class QuestionScreen extends Actor
 {
-    /**
+     /**
      * Act - do whatever the QuestionScreen wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act(){
-        if(Greenfoot.mouseClicked(this)){
-            if((getWorld().getObjects(ExplanationText.class).toArray()).length > 0)
-            {   System.out.println("Entered" + getWorld().getObjects(ExplanationText.class));
-                getWorld().removeObjects(getWorld().getObjects(ExplanationText.class));
-                getWorld().removeObjects(getWorld().getObjects(TextField.class));
-                getWorld().removeObjects(getWorld().getObjects(Question.class));
-                getWorld().removeObjects(getWorld().getObjects(QuestionScreen.class));
-                
-            }
-        }    
-        
+  
+           
+      
     }    
 }
