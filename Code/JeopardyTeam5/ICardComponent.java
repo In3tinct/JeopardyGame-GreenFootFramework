1dc6bf66fd768eadc50b993d55ca30f78c82d912
ajay <ajaymodi12@gmail.com>
2016-04-23 22:49:46 -0700

    command

--- Code/JeopardyTeam5/ICardComponent.java
+++
@@ -1,7 +0,0 @@
-interface ICardComponent
-{
-    public void addQCard(ICardComponent card);
-    public void removeQCard(ICardComponent card);
-    public void displayQCard(ICardComponent card);
-    public ICardComponent getQCard(int pos);
-};
