4dd6ae2a68a30900fe945590d805bfac2bbdc6bd
natashadsouza <natasha.dsouza2702@gmail.com>
2016-04-29 12:17:05 -0700

    Added files via upload

---
+++ Code/JeopardyTeam5/Subject.java
@@ -0,0 +1,10 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+public interface Subject {
+
+public void attach(Observer obj);
+
+
+public void detach(Observer obj);
+
+
+}
