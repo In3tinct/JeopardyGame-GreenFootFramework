a31aecd567279d54ce5d7d3a540dd52d9ed4f77d
natashadsouza <natasha.dsouza2702@gmail.com>
2016-04-30 13:47:24 -0700

    Update ClosedCard273.java

--- Code/JeopardyTeam5/ClosedCard273.java
+++ Code/JeopardyTeam5/ClosedCard273.java
@@ -26,10 +26,10 @@ public class ClosedCard273 extends QuestionCardLeaf273 implements ICardState {
 			if(cats.equalsIgnoreCase(q[i].cat) && (Integer.parseInt(score))==q[i].value)
 			{
 			world.addObject(new Question(q[i].que,card),305,60);
-			world.addObject(new TextField(q[i].answers.get(0).ans,card,q[i].answers.get(0).isCorrect),91,191);
-			world.addObject(new TextField(q[i].answers.get(1).ans,card,q[i].answers.get(1).isCorrect),91,247);
-			world.addObject(new TextField(q[i].answers.get(2).ans,card,q[i].answers.get(2).isCorrect),91,302);
-			world.addObject(new TextField(q[i].answers.get(3).ans,card,q[i].answers.get(3).isCorrect),91,356);
+			world.addObject(new TextField(q[i].answers.get(0).ans,card,q[i].answers.get(0).isCorrect,score),91,191);
+            world.addObject(new TextField(q[i].answers.get(1).ans,card,q[i].answers.get(1).isCorrect,score),91,247);
+            world.addObject(new TextField(q[i].answers.get(2).ans,card,q[i].answers.get(2).isCorrect,score),91,302);
+            world.addObject(new TextField(q[i].answers.get(3).ans,card,q[i].answers.get(3).isCorrect,score),91,356);
 			world.answerClicked=false;
 			world.startTimer();
 		   ((QuestionCardLeaf273)card).setCardState(new OpenCard273());
