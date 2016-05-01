c92a155725c42154fe726d7795aa06c725204142
TusharaA <tushara.sies@gmail.com>
2016-04-30 23:01:05 -0700

    refactored observer pattern

--- Code/JeopardyTeam5/Observer.java
+++ Code/JeopardyTeam5/Observer.java
@@ -1,6 +1,6 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public interface Observer {
  
-	public abstract void update();
+	public void update(int point);
 }
  
