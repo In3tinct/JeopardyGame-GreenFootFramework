8adb09a991dae4767e4ebbc7cc950ec290443aab
ajaycodes <ajaymodi12@gmail.com>
2016-04-30 22:57:04 -0700

    update clodedcard202
    final update clodedcard202

--- Code/JeopardyTeam5/ClosedCard202.java
+++ Code/JeopardyTeam5/ClosedCard202.java
@@ -26,10 +26,10 @@ public class ClosedCard202 extends QuestionCardLeaf202 implements ICardState {
             if(cats.equalsIgnoreCase(q[i].cat) && (Integer.parseInt(score))==q[i].value)
             {
             world.addObject(new Question(q[i].que,card),305,60);
-           world.addObject(new TextField(q[i].answers.get(0).ans,card,q[i].answers.get(0).isCorrect,score),91,191);
-            world.addObject(new TextField(q[i].answers.get(1).ans,card,q[i].answers.get(1).isCorrect,score),91,247);
-            world.addObject(new TextField(q[i].answers.get(2).ans,card,q[i].answers.get(2).isCorrect,score),91,302);
-            world.addObject(new TextField(q[i].answers.get(3).ans,card,q[i].answers.get(3).isCorrect,score),91,356);
+            world.addObject(new TextField(q[i].answers.get(0).ans,card,q[i].answers.get(0).isCorrect,Integer.parseInt(score)),91,191);
+            world.addObject(new TextField(q[i].answers.get(1).ans,card,q[i].answers.get(1).isCorrect,Integer.parseInt(score)),91,247);
+            world.addObject(new TextField(q[i].answers.get(2).ans,card,q[i].answers.get(2).isCorrect,Integer.parseInt(score)),91,302);
+            world.addObject(new TextField(q[i].answers.get(3).ans,card,q[i].answers.get(3).isCorrect,Integer.parseInt(score)),91,356);
             world.answerClicked=false;
             world.startTimer();
            ((QuestionCardLeaf202)card).setCardState(new OpenCard202());
@@ -49,7 +49,7 @@ public class ClosedCard202 extends QuestionCardLeaf202 implements ICardState {
 
     @Override
     public void resetCard() {
-        System.out.println("Reset closed Card");
+        //System.out.println("Reset closed Card");
         setCardState(new ClosedCard202());
 
     }
